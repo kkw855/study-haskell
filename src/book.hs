de019694ef0b6ba5893d6b7c7010c64ce49c89fe
kkw855 <kkw855@gmail.com>
2022-07-14 17:46:56 +0900

    Working...

---
+++ src/book.hs
@@ -0,0 +1,30 @@
+double' :: Num a => a -> a
+double' x = x + x
+
+doubleTwice' :: Num a => a -> a
+doubleTwice' x = double' (double' x)
+
+sum' :: Num a => [a] -> a
+sum' [] = 0
+sum' (x : xs) = x + sum' xs
+
+-- product' [2,3,4] “ 24
+product' :: Num a => [a] -> a
+product' [] = 1
+product' (x : xs) = x * product' xs
+
+-- qsort [3,5,1,4,2] = [1,2,3,4,5]
+qsort :: Ord a => [a] -> [a]
+qsort [] = []
+qsort (x : xs) = qsort smaller ++ [x] ++ qsort larger
+  where
+    smaller = [a | a <- xs, a <= x]
+    larger = [b | b <- xs, b > x]
+
+-- qsortReverse [3,5,1,4,2] = [5,4,3,2,1]
+qsortReverse :: Ord a => [a] -> [a]
+qsortReverse [] = []
+qsortReverse (x : xs) = qsortReverse larger ++ [x] ++ qsortReverse smaller
+  where
+    smaller = [a | a <- xs, a <= x]
+    larger = [b | b <- xs, b > x]
